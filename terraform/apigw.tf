5dc021a3a921ece229c9c5b37c1989f98f4ced58
Wiliam Hatimine <wiliamhatimine@gmail.com>
2025-05-12 00:28:11 -0300

    Fixing data file

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -4,7 +4,6 @@ resource "local_file" "openapi_template" {
   content = templatefile("${path.module}/openapi/tech-challenge-api-definition.json", {
     lb_dns_name            = data.aws_lb.nlb.dns_name
     vpc_link_id            = aws_api_gateway_vpc_link.vpc_link.id
-    authorizer_uri         = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${data.aws_lambda_function.lambda_authorizer.arn}/invocations"
     authorizer_credentials = data.aws_iam_role.lab_role.arn
   })
 }
@@ -17,19 +16,6 @@ resource "aws_api_gateway_rest_api" "web_api" {
   binary_media_types = ["*/*"]
 
   depends_on = [local_file.openapi_template]
-}
-
-data "aws_api_gateway_authorizers" "lambda_authorizer" {
-  rest_api_id = aws_api_gateway_rest_api.web_api.id
-}
-
-resource "aws_lambda_permission" "allow_api_gateway" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = var.lambda_authorizer_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.web_api.id}/authorizers/${data.aws_api_gateway_authorizers.lambda_authorizer.id}"
 }
 
 resource "aws_api_gateway_deployment" "web_api_deployment" {
