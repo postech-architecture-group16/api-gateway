4a004b50b10cdaaadc03d9424c7238ed90984753
Wiliam Hatimine <wiliamhatimine@gmail.com>
2025-05-12 00:23:29 -0300

    Creation of api gateway

---
+++ terraform/data.tf
@@ -0,0 +1,22 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
+}
+
+data "aws_lb" "nlb" {
+  name = var.nlb_name
+}
+
+data "aws_lb_listener" "nlb_listener" {
+  load_balancer_arn = data.aws_lb.nlb.arn
+  port              = 80
+}
+
+data "aws_security_group" "alb_sg" {
+  id = tolist(data.aws_lb.nlb.security_groups)[0]
+}
+
+data "aws_lambda_function" "lambda_authorizer" {
+  function_name = var.lambda_authorizer_name
+}
